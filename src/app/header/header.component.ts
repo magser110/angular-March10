e72dc32dc38276dcd614ffd4351afd0ffa4a6a4f
maggie serrano <https://github.com/magser110/October2024.git>
2025-03-06 19:40:23 -0500

    saving homework reading exercise

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   title = 'welcome to angular';
+  //description = 'this is a quick intro to angular components';
+  inputText = '';
+
+  resetInput() {
+    this.inputText = '';
+  }
+
+  showMessage = false;
+  toggleMessage() {
+    this.showMessage = !this.showMessage;
+  }
 }
